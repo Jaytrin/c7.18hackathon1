f4f154daa6e35dfae509dbb18e1c56ae9f228f90
Torie Pham <tientp@uci.edu>
2018-08-22 11:55:44 -0700

    created a players object

---
+++ player.js
@@ -0,0 +1,12 @@
+var players = [
+    {
+      symbol: 'X',
+      'class': 'xplayer',
+      color: white
+    },
+    {
+      symbol: 'O',
+      'class': 'oplayer',
+      color: white
+    }
+  ]
