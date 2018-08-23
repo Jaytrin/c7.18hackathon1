7b8962312d9ed1da1b2beb1a55767021d005634c
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-08-22 18:55:05 -0700

    reset v0.0

--- main.js
+++ main.js
@@ -5,9 +5,28 @@ function startGame(){
 }
 
 function clickHandler(){
-    $('.tile').click();//put a function name in click()
-    $('.reset').click();//put reset function in click();
+    $('.tile').click(test);//put a function name in click()
+    $('.reset').click(reset);//put reset function in click();
     $('#start').click();//need to put a start game function here
     $('.boardSize').click();//changeBoardSize and parameters go in here
     $('.winCondition').click();//put setWinCondition and parameters here
+}
+
+function test() {
+    var pressBoard= $(event.currentTarget) ;
+    playerWho = changePlayer();
+    //playerWho = '0';
+    //
+    if ( playerWho === 'X') {
+        pressBoard.text('X');
+    } else{
+        pressBoard.text('0');
+    }
+}
+
+function reset() {
+    // board clear
+    var boardTile = $(".tile") ;
+    boardTile.text('');
+
 }
